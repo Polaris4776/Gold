7f46ab69729f466691f529aa8344bf707e763ead
Polaris4776 <PolarisLink@protonmail.com>
2022-02-08 20:26:51 +0100

    Le json est lisible et fusionnable. On va merge la branche json et dev

--- language/read_json.py
+++ language/read_json.py
@@ -2,10 +2,13 @@ import json
 
 with open('language/fr.json') as json_file:
     data = json.load(json_file)
-shop = data["SHOP"]
+SHOP = data["SHOP"]
 
-print(data)
-print(type(data))
+print(SHOP)
+print(type(SHOP))
+print("\n\n")
+print(SHOP[0])
+print(SHOP[0]["name"])
 # data = json.dumps(data, ensure_ascii=False, indent=4)
 
 # print(data)
